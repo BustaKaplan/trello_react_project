0a2e46944fc0b3458036f2aa4e7cda5eef83dbdf
BustaKaplan <chelsea.kaplan3@gmail.com>
2017-07-20 21:13:44 -0500

    completed challange number two-m

--- src/components.jsx
+++ src/components.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-/* Define Card Component Here*/
-
-/* Column Component */
-class CardColumn extends React.Component {
-    render() {
-        return (<div></div>);
-    }
+class Card extends React.Component {
+  render(){
+    return(
+            <div className ="card">
+              <div className ="title">{this.props.title}</div>
+              <div className ="description">{this.props.description}</div>
+              <div className ="actions">
+                <button className ="delete-card">X</button>
+                <button className ="complete-card">></button>
+              </div>
+            </div>
+    );
+  }
 }
-
+class CardColumn extends React.Component {
+  _getCards(){
+    const cardList = [
+        {id:1, title:"Take React Course", description:"code on code on code on code..."},
+        {id: 2, title:"Learn React", description:"take the first level of React"},
+        {id:3, title:"Plan Two Truths and a Lie", description:"Finish Wireframe"}
+      ];
+    return cardList.map((card) => {
+      return (
+        <Card title={card.title} description={card.description} key={card.id} />
+     );
+   });
+  }
+  render() {
+    const cards = this._getCards();
+    console.log(this._getCards());
+    return (
+      <div id="up-next" className="card-column">
+        <div className="card-column-title">Up Next</div>
+        <div className="card-list">
+            {cards}
+        </div>
+    </div>
+  );
+  }
+}
 export default CardColumn;
